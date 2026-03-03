7982aa77b9fe75cb7bd1502b8fc1e132070f5012
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-03 17:53:48 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// importing the about.js and contact.js function from screens 
+import About from "./screens/about.js";
+import Contact from './screens/contact.js';
 
+// function App() {
+//   // return (
+//   //   <h1>Hello World</h1>
+//   // );
+
+//   //import function from screens about.js and contact.js  
+//   // return <About />;
+//   // return <Contact />;
+
+//  
+// }
+
+ // passing both screens
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      {/* passing components from screens to app.js */}
+      <About />
+      <Contact />
     </div>
   );
 }
 
 export default App;
+
+//1
+//Basic React App structure-Done Removerd unnecessary code and added boilerplate code to know we are using react and passing function from screens to app.js
+//screens and components created
+
+// 2 go about.js in screens folder
+
+//3
+//return <About />
+// imported About from screens so it can visibale in browser
+
+//4 basically this happend
+// screens folder created and about.js file created and inported in app.js
+
+//5 screens/contact.js created and ipmorting in app.js
+
+//6 contact and about js  fils added in app.js returning using div and calling both functions <About /> and <Contact /> passed as components
---
+++ src/screens/about.js
@@ -0,0 +1,13 @@
+//Boiler code in js to know js were using react and passing function
+//2 file creatted in screens in side screens about file created
+// imported tp app.js and rendered in app.js
+
+import React from 'react';
+// import Title from '../components/Title';
+
+function About() {
+    return <h1>this is screen,About Us</h1>;
+    // return <Title />
+}
+
+export default About;
